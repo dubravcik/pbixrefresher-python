c10ce9d442d964e7872e04e8b4c92762a6ac48f5
Michal Dubravcik <michal.dubravcik@gmail.com>
2017-12-11 16:38:55 +0100

    fix waiting

--- pbixrefresher/pbixrefresher.py
+++ pbixrefresher/pbixrefresher.py
@@ -2,27 +2,29 @@ import time
 import os
 import argparse
 import psutil
-from pywinauto.application import Application, win32defines
+from pywinauto.application import Application
 from pywinauto import timings
-from pywinauto.win32functions import SetForegroundWindow, ShowWindow
-
 
 
 def type_keys(string, element):
     """Type a string char by char to Element window"""
     for char in string:
-        element.type_keys(char, set_foreground=False)
+        element.type_keys(char)
 
      
 # Parse arguments from cmd
 parser = argparse.ArgumentParser()
 parser.add_argument("workbook", help = "Path to .pbix file")
-parser.add_argument("--workspace", default = "My workspace")
+parser.add_argument("--workspace", help = "name of online Power BI service work space to publish in", default = "My workspace")
+parser.add_argument("--refresh-timeout", help = "refresh timeout", default = 30000)
+parser.add_argument("--init-wait", help = "initial wait time on startup", default = 15)
 args = parser.parse_args()
 
 timings.after_clickinput_wait = 1
 WORKBOOK = args.workbook
 WORKSPACE = args.workspace
+INIT_WAIT = args.init_wait
+REFRESH_TIMEOUT = args.refresh_timeout
 
 # Kill running PBI
 PROCNAME = "PBIDesktop.exe"
@@ -35,32 +37,32 @@ time.sleep(3)
 # Start PBI and open the workbook
 print("Starting Power BI")
 os.system('start "" "' + WORKBOOK + '"')
-time.sleep(15)
+time.sleep(INIT_WAIT)
 
 # Connect pywinauto
 print("Identifying Power BI window")
 app = Application(backend = 'uia').connect(path = PROCNAME)
 win = app.window(title_re = '.*Power BI Desktop')
-time.sleep(2)
+time.sleep(5)
 win.wait("enabled", timeout = 300)
-
-#win.maximize()
+win.Save.wait("enabled", timeout = 300)
 win.set_focus()
-win.Ribbon.click_input()
+win.Home.click_input()
+win.Save.wait("enabled", timeout = 300)
 win.wait("enabled", timeout = 300)
 
 # Refresh
 print("Refreshing")
 type_keys("%y01y14", win)
 #wait_win_ready(win)
-time.sleep(2)
+time.sleep(5)
 win.wait("enabled", timeout = 300)
 
 # Save
 print("Saving")
 type_keys("%1", win)
 #wait_win_ready(win)
-time.sleep(2)
+time.sleep(5)
 win.wait("enabled", timeout = 300)
 
 # Publish
@@ -75,7 +77,7 @@ except Exception:
     pass
 if win.Replace.exists():
     win.Replace.click_input()
-win["Got it"].wait('visible', timeout = 3000)
+win["Got it"].wait('visible', timeout = REFRESH_TIMEOUT)
 win["Got it"].click_input()
 
 #Close
